fcddacf933002ae3879fc7ec98702bfe44386736
Phương Mai Anh <phuongmaianh122006@gmail.com>
2026-09-07 08:31:20 +0700

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTWLesson04.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "Mai Anh";
+            ViewData["age"] = 20;
+            TempData["address"] = "Ha Noi";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
